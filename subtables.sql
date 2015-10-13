d909a80ce27a744ab2e7bb4906a729fcdcad7324
tree101 <tree101@stanford.edu>
2015-10-13 09:43:04 -0700

    minor changes; added drop cascades

--- subtables.sql
+++ subtables.sql
@@ -2,12 +2,14 @@
 --- sub-tables
 
 -- drop tables 
-DROP TABLE  IF EXISTS  subject;
+DROP TABLE  IF EXISTS  subject CASCADE;
 -- CREATE EXTENSION "uuid-ossp";
-DROP TABLE  IF EXISTS  version;
-DROP TABLE  IF EXISTS  sample;
+DROP TABLE  IF EXISTS  version CASCADE;
+DROP TABLE  IF EXISTS  sample CASCADE;
+
 
 DROP TABLE  IF EXISTS  type;
+DROP TABLE  IF EXISTS  subtype;
 DROP TABLE  IF EXISTS  consent;
 DROP TABLE  IF EXISTS  unit;
 DROP TABLE  IF EXISTS  action;
@@ -16,21 +18,11 @@ DROP TABLE  IF EXISTS  location_collection;
 DROP TABLE  IF EXISTS  storage;
 DROP TABLE  IF EXISTS  box_name;
 
-
-CREATE TABLE type(
-id SERIAL PRIMARY KEY, 
-tissue VARCHAR NOT NULL, -- tissue, molecular, library, etc
-subtype INT NOT NULL, -- FK subtype tissue: Lung, Kidney, Blood, etc  molecular: RNA, DNA, Protein
-notes VARCHAR
-);
-
-DROP TABLE  IF EXISTS  subtype;
-
-CREATE TABLE subtype(
-id SERIAL PRIMARY KEY, 
-subtype INT NOT NULL, -- FK subtype tissue: Lung, Kidney, Blood, etc  molecular: RNA, DNA, Protein
-notes VARCHAR
-);
+-- drop tables 
+DROP TABLE  IF EXISTS  subject;
+-- CREATE EXTENSION "uuid-ossp";
+DROP TABLE  IF EXISTS  version;
+DROP TABLE  IF EXISTS  sample;
 
 
 -----------------------------------------------------------------------
@@ -41,14 +33,12 @@ notes VARCHAR
 
 CREATE TABLE consent(
 id SERIAL PRIMARY KEY, 
-name VARCHAR NOT NULL, 
+form VARCHAR NOT NULL, 
 notes VARCHAR
 );
 
 -----------------------------------------------------------------------
 --- any type of unit you want here 
-
-
 
 CREATE TABLE unit(
 id SERIAL PRIMARY KEY, 
@@ -57,27 +47,37 @@ notes VARCHAR
 );
 
 
+CREATE TABLE subtype(
+id SERIAL PRIMARY KEY, 
+subtype VARCHAR NOT NULL, --
+notes VARCHAR
+);
+
+CREATE TABLE type(
+id SERIAL PRIMARY KEY, 
+tissue VARCHAR NOT NULL, -- tissue, molecular, etc
+subtype INT NOT NULL REFERENCES  subtype(id), -- FK subtype tissue: Lung, Kidney, Blood, etc  molecular: RNA, DNA, Protein
+notes VARCHAR
+);
+
 
 -----------------------------------------------------------------------
 --- action table defines what the user did 
 
 
-
 CREATE TABLE action(
 id SERIAL PRIMARY KEY, 
-action VARCHAR NOT NULL, -- so far we have modify, create, withdraw, add, transfer
-notes VARCHAR NOT NULL
+action VARCHAR NOT NULL, -- so far we have modify, create, withdraw, add, transfer, end
+notes VARCHAR 
 );
 
 --- users, there will be a script that generates new users with permission level 
-
-
-
+--- login will store as cookie and entries will automatically use that username by default
 CREATE TABLE users(
 id uuid NOT NULL DEFAULT uuid_generate_v4(), 
 dateadded  TIMESTAMP(2) DEFAULT (now() at time zone 'PST'), -- date added to database
 permission INT NOT NULL DEFAULT (0), -- default read only, 1, 2, 3 will have different permission levels
-notes VARCHAR NOT NULL
+notes VARCHAR
 );
 
 --- create a location of collection 
@@ -89,13 +89,18 @@ CREATE TABLE location_collection(
 id SERIAL PRIMARY KEY, 
 location_collection VARCHAR NOT NULL, --  eg SIM1, Stanford Hospital, etc
 room VARCHAR,
-notes VARCHAR NOT NULL
+notes VARCHAR
 );
 
 
 --- predefined strorage location 
+-- subtable for storage 
 
-
+CREATE TABLE box_name(
+id SERIAL PRIMARY KEY, 
+name VARCHAR NOT NULL,
+notes VARCHAR 
+);
 
 CREATE TABLE storage(
 id SERIAL PRIMARY KEY, 
@@ -103,15 +108,7 @@ building VARCHAR NOT NULL,
 room VARCHAR NOT NULL,
 storage_name VARCHAR NOT NULL, -- eg -80b, -20a, 4a
 shelf VARCHAR, -- 
-box VARCHAR , -- FK box_name  
-notes VARCHAR NOT NULL
+box INT REFERENCES box_name(id), -- FK box_name  
+notes VARCHAR
 );
 
--- subtable for storage 
-
-
-CREATE TABLE box_name(
-id SERIAL PRIMARY KEY, 
-name VARCHAR NOT NULL,
-notes VARCHAR 
-);
--- tables_.sql
+++ tables_.sql
@@ -9,10 +9,10 @@
 --- unique to the old record
 
 
-DROP TABLE  IF EXISTS  subject;
+DROP TABLE  IF EXISTS  subject CASCADE;
 -- CREATE EXTENSION "uuid-ossp";
-DROP TABLE  IF EXISTS  version;
-DROP TABLE  IF EXISTS  sample;
+DROP TABLE  IF EXISTS  version CASCADE;;
+DROP TABLE  IF EXISTS  sample CASCADE;;
 
 
 
@@ -23,22 +23,23 @@ age INT,
 sex INT, -- male, female, na
 consent INT NOT NULL REFERENCES  consent(id),  -- FK from conset table
 date_create DATE NOT NULL,
-date_modify date, -- this date is the most current version date
+date_modify DATE, -- this date is the most current version date
 version INT, -- inserts latest version but keeps a record in table "version"
-action INT REFERENCES action (id), -- FK table action
+action INT REFERENCES action (id), -- FK table action (this fields tells the users what was the latest action on this record)
+								   -- we can extract all records from version table	
 notes VARCHAR
 );
 
 
 --____________________________________________________________________________________
----- combined sample tables will consist of generic fields but contain a JSON field 
----- specific to any sort of data 
+---- combined sample tables will consist of generic fields but contain a JSON field specific to
+---- whatever the data maybe so for example if this was a DNA entry perhaps it would also contain 280/260 ratio 
 ---- versions and log stored in table version
 
 
 CREATE TABLE sample(
     id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), -- PK
-    id_subject INT NOT NULL REFERENCES  subject(id), -- FK from subject 
+    id_subject uuid NOT NULL REFERENCES  subject(id), -- FK from subject 
     
 	tissue_type INT NOT NULL REFERENCES  type(id), -- FK from type
 	sub_type INT NOT NULL REFERENCES  subtype(id), -- FK from subtype
@@ -54,8 +55,9 @@ CREATE TABLE sample(
 	unit INT NOT NULL, --- FK from units
 	location_storage INT NOT NULL REFERENCES  storage(id), -- FK from storage
     
+	label VARCHAR NOT NULL, -- this is still to be decided contingent on what labeling machine we need
 
-    date_modify date, -- current entry date
+
     
 	version INT, -- inserts latest version but keeps a record in table "version"
     action INT REFERENCES action (id), -- FK table action
@@ -74,11 +76,11 @@ CREATE TABLE sample(
 
 CREATE TABLE version(
 calling_table VARCHAR, -- name of calling table
-id uuid NOT NULL, -- primary id of calling table
-version_number SERIAL NOT NULL
-action INT, -- action from calling table
+id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), -- primary id of calling table
+version_number INT NOT NULL, -- script will figure this out
+action INT REFERENCES action (id), -- action from calling table
 users INT, -- from current user (store in cookie on login)
-timestamp TIMESTAMP(2), -- when triggered
+timestamp TIMESTAMP(2), -- when  was this triggered
 data_log JSON, -- stores all data associated with older entry Not dependent on table
 notes VARCHAR
 );
