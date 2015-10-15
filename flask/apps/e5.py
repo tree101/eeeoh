58846fe9e2a3f5de4eed635e687542a573d7204d
tree101 <tree101@stanford.edu>
2015-10-15 16:17:23 -0700

    e5 better db handling

--- flask/apps/e5.py
+++ flask/apps/e5.py
@@ -1,3 +1,4 @@
+#!flask/bin/python
 from flask import Flask
 from flask import request
 from flask import render_template
@@ -6,6 +7,11 @@ from flask import redirect
 from flask import url_for
 from flask import escape
 from flask.ext.sqlalchemy import SQLAlchemy
+
+from urlparse import urlparse, urljoin
+from flask.ext.wtf import Form
+
+
 import psycopg2
 
 app = Flask(__name__)
@@ -35,14 +41,25 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
-@app.route('/db')
+@app.route('/db', methods=['GET', 'POST'])
 def db_get():
     # remove the username from the session if it's there
 		cur = conn.cursor()
+		mydb = request.args.get("db")
+		#SELECT * from action
+		#SELECT * from action
 		
-		cur.execute("""SELECT * from action""")
+		mydb = 'SELECT * from '+ mydb; 
+		cur.execute(mydb)
 		rows = cur.fetchall()
-		return 'hey dude <hr> %s' % rows	
+		out = '';
+		for row in rows:
+			#out = row[1]
+			out = out+ '<tr>'+' '.join('<td>{0}</td>'.format(w) for w in row)+'</tr>'
+	     
+		#out = '<table>' + out + '</table>'
+		#dbc = '<table></table>';
+		return render_template('table.html',title='',table_stuff=out)
 	
 # set the secret key.  keep this really secret:
 app.secret_key = 'EEZr98j/*yX R~XHH!jmN]0)X/,?RT'
