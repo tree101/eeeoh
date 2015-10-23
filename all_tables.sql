39f6911fe7e210dc26a36d6951586a7bacc69d14
tree101 <tree101@stanford.edu>
2015-10-23 11:28:33 -0700

    combined tables

---
+++ all_tables.sql
@@ -0,0 +1,241 @@
+
+
+
+DROP TABLE  IF EXISTS  projects CASCADE;
+CREATE TABLE projects(
+id SERIAL PRIMARY KEY, 
+name VARCHAR NOT NULL,
+groupName VARCHAR, 
+notes TEXT 
+
+);
+
+
+-- junction table here 
+DROP TABLE  IF EXISTS  userlogin_projects CASCADE;
+CREATE TABLE userlogin_projects(
+project_id INT REFERENCES projects(id),
+userLogin_id INT REFERENCES userLogin(id), 
+PRIMARY KEY (project_id, userLogin_id),
+notes TEXT 
+);
+
+-- uers 
+CREATE TYPE usertype AS ENUM ('admin', 'power', 'view', 'other');
+DROP TABLE  IF EXISTS  userlogin CASCADE;
+CREATE TABLE userLogin(
+id SERIAL PRIMARY KEY, 
+firstname VARCHAR NOT NULL,
+lastname VARCHAR NOT NULL, 
+email VARCHAR NOT NULL,
+password VARCHAR, 
+usertype usertype,
+notes TEXT, 
+
+)
+
+
+
+-- cosents 
+
+DROP TABLE  IF EXISTS  consent CASCADE;
+CREATE TABLE consent(
+id SERIAL PRIMARY KEY, 
+form VARCHAR NOT NULL, 
+link VARCHAR, -- this could be a hyperlink of some sort to the actual document
+notes TEXT
+);
+
+DROP TABLE  IF EXISTS  subject_consent CASCADE;
+CREATE TABLE subject_consent(
+subject_id uuid NOT NULL REFERENCES subject(id), 
+consent_id INT NOT NULL REFERENCES consent(id), 
+PRIMARY KEY (subject_id, consent_id),
+notes TEXT
+);
+
+-- diagnosis 
+
+DROP TABLE  IF EXISTS  diagnosis CASCADE;
+CREATE TABLE diagnosis(
+id SERIAL PRIMARY KEY, 
+disease VARCHAR NOT NULL, 
+notes TEXT
+);
+
+-- junction 
+
+DROP TABLE  IF EXISTS  subject_diagnosis CASCADE;
+CREATE TABLE subject_diagnosis(
+diagnosis_id INT NOT NULL REFERENCES diagnosis(id), 
+subject_id uuid  NOT NULL REFERENCES subject(id),
+PRIMARY KEY (diagnosis_id, subject_id),
+notes TEXT
+);
+
+-- subject table 
+
+CREATE TYPE sex AS ENUM ('M', 'F', 'UNK', 'OTHER');
+DROP TABLE  IF EXISTS  subject CASCADE;
+CREATE TABLE subject(
+id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), 
+users VARCHAR NOT NULL, --from current user (store in session cookie on login)
+age INT,
+sex sex NOT NULL,
+timestamp TIMESTAMP(2),
+project_id INT REFERENCES projects(id),
+version INT, 
+notes TEXT
+);
+
+-- sample 
+
+-- ** keeps track of where each samples orignates from 
+
+DROP TABLE  IF EXISTS  parentID CASCADE;
+
+CREATE TABLE parentID(	
+	child  uuid,
+	parent uuid,
+	PRIMARY KEY (child, parent)
+	);
+	
+-- ** sample  
+
+DROP TABLE  IF EXISTS  subtype CASCADE;
+DROP TABLE  IF EXISTS  sampletype CASCADE;
+
+CREATE TABLE subtype(
+id SERIAL PRIMARY KEY, 
+subtype VARCHAR NOT NULL, -- subtype tissue: Lung, Kidney, Blood, etc  molecular: RNA, DNA, Protein
+notes TEXT
+);
+
+CREATE TABLE sampletype(
+id SERIAL PRIMARY KEY, 
+tissue VARCHAR NOT NULL, -- tissue, molecular, etc
+subtype_id INT NOT NULL REFERENCES  subtype(id), -- 
+notes TEXT
+);	
+
+-- units can be anything you want 
+DROP TABLE  IF EXISTS  unit CASCADE;
+CREATE TABLE unit(
+id SERIAL PRIMARY KEY, 
+unit VARCHAR NOT NULL, 
+notes TEXT
+);
+
+-- storage locations 
+
+DROP TABLE  IF EXISTS  institution CASCADE;
+
+
+CREATE TABLE institution(
+id SERIAL PRIMARY KEY, 
+name VARCHAR NOT NULL,
+notes TEXT 
+);
+DROP TABLE  IF EXISTS  building CASCADE;
+CREATE TABLE building(
+id SERIAL PRIMARY KEY, 
+name VARCHAR NOT NULL,
+notes TEXT 
+);
+DROP TABLE  IF EXISTS  room CASCADE;
+CREATE TABLE room(
+id SERIAL PRIMARY KEY, 
+name VARCHAR NOT NULL,
+notes TEXT 
+);
+
+DROP TABLE  IF EXISTS  storageUnit CASCADE;
+CREATE TABLE storageUnit(
+id SERIAL PRIMARY KEY, 
+name VARCHAR NOT NULL,
+notes TEXT 
+);
+
+DROP TABLE  IF EXISTS  shelf CASCADE;
+CREATE TABLE shelf(
+id SERIAL PRIMARY KEY, 
+name VARCHAR NOT NULL,
+notes TEXT 
+);
+
+DROP TABLE  IF EXISTS  box CASCADE;
+CREATE TABLE box(
+id SERIAL PRIMARY KEY, 
+name VARCHAR NOT NULL,
+notes TEXT 
+);
+
+-- position really depends on what the intended storage is. 
+DROP TABLE  IF EXISTS  position CASCADE;
+CREATE TABLE position(
+id SERIAL PRIMARY KEY, 
+name VARCHAR NOT NULL,
+notes TEXT 
+);
+
+DROP TABLE  IF EXISTS  location CASCADE;
+CREATE TABLE location(
+id SERIAL PRIMARY KEY, 
+            
+institution_name INT NOT NULL REFERENCES  institution(id),
+building_name INT REFERENCES building(id), 
+room_name INT REFERENCES room(id),
+storageUnit_name INT REFERENCES storageUnit(id),
+shelf_name INT REFERENCES shelf(id),  
+box_name INT REFERENCES box(id),   
+notes TEXT
+);
+
+DROP TABLE  IF EXISTS  sample CASCADE;
+CREATE TABLE sample(
+    id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), 
+    subject_id uuid NOT NULL REFERENCES  subject(id),  
+	sampletype_id INT NOT NULL REFERENCES  sampletype(id), -- FK from type
+	timestamp TIMESTAMP(2),	
+	
+    users INT NOT NULL, -- from cookie
+    amount double precision NOT NULL, 
+	
+	unit_id INT REFERENCES  unit(id), 
+	
+	location_id INT NOT NULL REFERENCES  location(id), 
+    
+
+	label VARCHAR NOT NULL, -- this is still to be decided contingent on what labeling machine we need
+
+
+	parentID_id uuid REFERENCES sample(id),
+	project_id INT REFERENCES projects(id),
+    notes TEXT	
+
+	);
+	
+-- end sample table creations 
+
+-- logging tables here 
+DROP TABLE  IF EXISTS  version;
+CREATE TABLE version(
+calling_table VARCHAR, -- name of calling table
+id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(), -- primary id of calling table
+version_number INT NOT NULL, -- script will figure this out
+users INT, -- from current user (store in cookie on login)
+timestamp TIMESTAMP(2), -- when  was this triggered
+data_log JSON, -- stores only what was changed 
+notes TEXT
+);
+DROP TABLE  IF EXISTS  logger;
+CREATE TABLE logger(
+id SERIAL PRIMARY KEY,
+timestamp TIMESTAMP(2), -- when  was this triggered
+lognotes TEXT
+);
+
+	
+
+
+
