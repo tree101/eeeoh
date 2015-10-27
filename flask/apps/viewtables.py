fe6f322112928795505420e139e21dbc7eb0ff51
tree101 <tree101@stanford.edu>
2015-10-26 17:08:23 -0700

    Add admin checks, inserts, view, delete

--- flask/apps/viewtables.py
+++ flask/apps/viewtables.py
@@ -59,7 +59,7 @@ def table_user():
     return redirect(url_for('login'))
 
 
-@app.route('/table_projects')
+@app.route('/table_projects', methods=['GET', 'POST'])
 def table_projects():
     # check if user is login.  
     if 'username' in session:
@@ -68,17 +68,39 @@ def table_projects():
 		admin = checkuser(cur, session['username'])
 		if (admin != 1):
 			return render_template("error.html",error="you need to be a amdiminstrator to access this",name=escape(session['username'])   )
+		# now check if this is view only or add record then view 
+		action = int(request.args.get('action'))
+		# add record if action ==1 
+		# add record if action == -1
+		whatdidyoudo = ''
+		if action == 1:
+			
+			name_project =  request.args.get('name')
+			group_name   =  request.args.get('groupname')
+			notes =         request.args.get('notes')
+			cInsert = ('INSERT INTO projects (name, groupname, notes) VALUES (%s, %s,%s);')
+			cur.execute(cInsert, (name_project,group_name,notes))
+			conn.commit()
+			whatdidyoudo = "You just inserted %s %s into table projects" % (name_project,group_name) 
+		# delete here instead
+		if action == -1:
+			id = request.args.get('id')
+			delete_this = 'DELETE FROM projects WHERE id = %s;';
+			cur.execute(delete_this, (id,)) 
+			conn.commit()
+			whatdidyoudo = "You just deleted id %s from table projects " % (id,) 
 		numrowstart = int(request.args.get('numrows'))
 		numrowend = numrowstart+ 50
-		head = ['ID','Name','Group Name','Notes']
+		head = ['ID','Name','Group Name','Notes','Delete']
 		
 		c = 'SELECT id, name, groupname,notes FROM projects LIMIT %s OFFSET %s;';
 		cur.execute(c, (numrowend,numrowstart)) 
 		rows = cur.fetchall()
 		#rows.append(head)
-		rows.insert(0,head)
+		#rows.insert(0,head)
+		# lets not do the above; its easier but to stringent, instead pass header info as a seperate var
 
-		return render_template("table_view.html",data=rows,name=escape(session['username']))
+		return render_template("table_view.html",data=rows,name=escape(session['username']),headinfo= head, whatdidyoudo=whatdidyoudo)
     return redirect(url_for('login'))
 	
 	
